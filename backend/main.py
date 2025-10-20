f3c894357f132b91b757d636e7f313fc4cda42d1
reiden26 <rheiden77@gmail.com>
2025-10-20 13:35:13 -0500

    Add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,7 @@ models.Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://proyecto2-sensores-esp32.vercel.app",
         "http://localhost:4200",
         "http://localhost:5173",
         "http://127.0.0.1:4200",
