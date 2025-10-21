6f4395cd2ee7ab516b5065f4e5c082eda83abced
reidorvin04-ops <reidorvin04@gmail.com>
2025-10-20 23:25:06 -0500

    Menu desplegable de la app web en movil

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +9,41 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class NavbarComponent {
   @Output() loginClick = new EventEmitter<void>();
   
+  isMobileMenuOpen = false;
+  
   onLoginClick() {
     this.loginClick.emit();
+  }
+  
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    // Prevenir scroll del body cuando el menú está abierto
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+  
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    document.body.style.overflow = '';
+  }
+  
+  // Cerrar menú móvil al cambiar el tamaño de la ventana
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth > 768) {
+      this.closeMobileMenu();
+    }
+  }
+  
+  // Cerrar menú móvil al hacer scroll
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    if (this.isMobileMenuOpen) {
+      this.closeMobileMenu();
+    }
   }
   
   scrollToTop(event: Event) {
